ab39990486843bff39cb698b12b716c3a37d7b2b
dries007 <admin@dries007.net>
2013-10-28 17:28:49 +0100

    Update .gitignore
    Included mcp folder that you need when using ChickenBonesCore.

--- src/main/java/ccm/yaim/YAIM.java
+++ src/main/java/ccm/yaim/YAIM.java
@@ -8,12 +8,5 @@ import cpw.mods.fml.common.Mod;
 @Mod(modid = Data.MODID, name = Data.MODID)
 public class YAIM
 {
-    public YAIM()
-    {
-        for (int i = -15; i < 15; i++)
-        {
-            float f = (float) (Math.pow(10, i) + Math.random() * 1000 * Math.pow(10, i));
-            System.out.println(f + ": " + SINumber.getMostAppropriate(TheMetricSystem.Unit.VOLTAGE, f).toString());
-        }
-    }
+
 }
